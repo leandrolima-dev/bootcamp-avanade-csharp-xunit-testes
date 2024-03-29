f8a6512ca559fc91238afdf6ab3d182c7ec6f433
Leandro Lima <lleandrorj@gmail.com>
2024-03-29 14:17:53 -0300

    feat: :sparkles: Implementando a classe Calculadora

--- Calculadora/Program.cs
+++ Calculadora/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Calculadora.Services;
+
+internal class Program
+{
+  private static void Main(string[] args)
+  {
+    CalculadoraImp calc = new();
+    int num1 = 10;
+    int num2 = 20;
+
+    Console.WriteLine($"{num1} + {num2} = {calc.Somar(num1, num2)}");
+  }
+}
